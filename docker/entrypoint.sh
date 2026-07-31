eeb3d3965e99ce8f25c89120e8b32338e257211c
romaina229 <nounonvenanromain@gmail.com>
2026-07-31 19:16:07 +0100

    vider tout

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,7 +14,7 @@ fi
 php artisan storage:link || true
 
 echo "==> Exécution des migrations (forcé temporairement)…"
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 echo "==> Exécution des seeders (forcé temporairement)…"
 php artisan db:seed --force
